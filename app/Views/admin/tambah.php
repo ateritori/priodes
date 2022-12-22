dec91caf5ed2cedce3a297c131c1d4e44f4928f4
Ubaidillah Aminuddin Thoyieb <ateritori@gmail.com>
2022-12-22 22:24:55 +0700

    22122234
    22122234

--- app/Views/admin/tambah.php
+++ app/Views/admin/tambah.php
@@ -3,6 +3,13 @@
 <main class="container">
     <div class="bg-light p-5 rounded">
         <h1>Tambah Kriteria</h1>
+        <form>
+            <div class="form-group mb-2">
+                <input type="text" class="form-control" id="namaKriteria" placeholder="Masukkan Nama Kriteria">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+            <a class="btn btn-secondary" href="<?= base_url(); ?>/kriteria">Kembali</a>
+        </form>
     </div>
 </main>
 <?php $this->endSection(); ?>
