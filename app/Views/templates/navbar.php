c9dd92f0e0dd26661d7a9f3b1a5f96c1edb70cc3
Ubaidillah Aminuddin Thoyieb <ateritori@gmail.com>
2023-01-23 20:47:07 +0700

    2301222047
    2301222047

--- app/Views/templates/navbar.php
+++ app/Views/templates/navbar.php
@@ -6,15 +6,15 @@
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
             <ul class="navbar-nav me-auto mb-2 mb-md-0">
                 <li class="nav-item">
                     <a class="nav-link" aria-current="page" href="<?= base_url(); ?>">Beranda</a>
                 </li>
                 <?php if (in_groups('admin')) : ?>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/users">Users</a>
-                    </li>
                     <li class="nav-item">
                         <a class="nav-link" href="/kriteria">Kriteria</a>
                     </li>
@@ -37,7 +37,15 @@
                     <a class="nav-link" href="/logout">Keluar</a>
                 </li>
             </ul>
-            <a class="navbar-brand" href="#"><?= user()->username; ?></a>
         </div>
+        <li class="nav-item dropdown">
+            <button class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                <?= user()->nama; ?>
+            </button>
+            <ul class="dropdown-menu dropdown-menu-dark">
+                <li><a class="dropdown-item" href="/logout">Log Out</a></li>
+            </ul>
+        </li>
+
     </div>
 </nav>
